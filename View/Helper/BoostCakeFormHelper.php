191b5ed1f10d71a17545da64d2e57152d4b3b544
Justin Slamka <slamkajs@github.com>
2013-10-07 12:47:47 -0400

    Added radio override

--- View/Helper/BoostCakeFormHelper.php
+++ View/Helper/BoostCakeFormHelper.php
@@ -195,6 +195,110 @@ class BoostCakeFormHelper extends FormHelper {
 		return $html;
 	}
 
+	public function radio($fieldName, $options = array(), $attributes = array()) {
+		$attributes = $this->_initInputField($fieldName, $attributes);
+
+		$showEmpty = $this->_extractOption('empty', $attributes);
+		if ($showEmpty) {
+			$showEmpty = ($showEmpty === true) ? __d('cake', 'empty') : $showEmpty;
+			$options = array('' => $showEmpty) + $options;
+		}
+		unset($attributes['empty']);
+
+		$legend = false;
+		if (isset($attributes['legend'])) {
+			$legend = $attributes['legend'];
+			unset($attributes['legend']);
+		} elseif (count($options) > 1) {
+			$legend = __(Inflector::humanize($this->field()));
+		}
+
+		$label = true;
+		if (isset($attributes['label'])) {
+			$label = $attributes['label'];
+			unset($attributes['label']);
+		}
+
+		$separator = null;
+		if (isset($attributes['separator'])) {
+			$separator = $attributes['separator'];
+			unset($attributes['separator']);
+		}
+
+		$between = null;
+		if (isset($attributes['between'])) {
+			$between = $attributes['between'];
+			unset($attributes['between']);
+		}
+
+		$value = null;
+		if (isset($attributes['value'])) {
+			$value = $attributes['value'];
+		} else {
+			$value = $this->value($fieldName);
+		}
+
+		$disabled = array();
+		if (isset($attributes['disabled'])) {
+			$disabled = $attributes['disabled'];
+		}
+
+		$out = array();
+
+		$hiddenField = isset($attributes['hiddenField']) ? $attributes['hiddenField'] : true;
+		unset($attributes['hiddenField']);
+
+		if (isset($value) && is_bool($value)) {
+			$value = $value ? 1 : 0;
+		}
+
+		foreach ($options as $optValue => $optTitle) {
+			$optionsHere = array('value' => $optValue);
+
+			if (isset($value) && strval($optValue) === strval($value)) {
+				$optionsHere['checked'] = 'checked';
+			}
+			$isNumeric = is_numeric($optValue);
+			if ($disabled && (!is_array($disabled) || in_array((string)$optValue, $disabled, !$isNumeric))) {
+				$optionsHere['disabled'] = true;
+			}
+			$tagName = Inflector::camelize(
+				$attributes['id'] . '_' . Inflector::slug($optValue)
+			);
+
+			if ($label) {
+				$optTitle = $this->Html->useTag('label', $tagName, '', $optTitle);
+			}
+			if (is_array($between)) {
+				$optTitle .= array_shift($between);
+			}
+			$allOptions = array_merge($attributes, $optionsHere);
+			$out[] = $this->Html->useTag('label', 
+				$tagName, 
+				array(), 
+				$this->Html->useTag('radio', $attributes['name'], $tagName,
+					array_diff_key($allOptions, array('name' => null, 'type' => null, 'id' => null)),
+					$optTitle));
+		}
+		$hidden = null;
+
+		if ($hiddenField) {
+			if (!isset($value) || $value === '') {
+				$hidden = $this->hidden($fieldName, array(
+					'id' => $attributes['id'] . '_', 'value' => '', 'name' => $attributes['name']
+				));
+			}
+		}
+		$out = $hidden . implode($separator, $out);
+
+		if (is_array($between)) {
+			$between = '';
+		}
+		if ($legend) {
+			$out = $this->Html->useTag('fieldset', '', $this->Html->useTag('legend', $legend) . $between . $out);
+		}
+		return $out;
+	}
 /**
  * Overwrite FormHelper::_selectOptions()
  * If $attributes['style'] is `<input type="checkbox">` then replace `<label>` position
